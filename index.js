b3d201d53cf2a9f82a43f124f96e21615cfdf143
MrRiaz <monsurrahmanriaz@gmail.com>
2020-10-15 23:54:37 +0600

    done

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x3yya.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+const port = 5000
+
+const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+var admin = require("firebase-admin");
+var serviceAccount = require("./configs/burj-al-arab1-firebase-adminsdk-k5npx-bd288afd8e.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIRE_DB
+});
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const bookings = client.db("burjAlArab").collection("bookings");
+
+  app.post('/addBooking', (req, res) => {
+    const newBooking = req.body;
+    bookings.insertOne(newBooking)
+      .then(result => {
+        res.send(insertedCount > 0);
+      })
+  })
+
+  app.get('/booking', (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith('Bearer ')) {
+      const idToken = bearer.split(' ')[1];
+      admin.auth().verifyIdToken(idToken)
+        .then(function (decodedToken) {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          if (tokenEmail == queryEmail) {
+            bookings.find({ email: queryEmail })
+              .toArray((err, documents) => {
+                res.status(200).send(documents);
+              })
+          }
+          else{
+            res.status(401).send('un-authoeize access');
+          }
+        }).catch(function (error) {
+          res.status(401).send('un-authoeize access');
+        });
+    }
+    else{
+      res.status(401).send('un-authoeize access');
+    }
+  })
+});
+
+app.listen(port);
